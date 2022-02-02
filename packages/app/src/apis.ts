a457860483d5c29d97ebe0c1a197717f9abda90f
Taras Mankovski <taras@frontside.com>
2022-02-01 16:16:14 -0800

    Added DetailList with data that comes from catalog api

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -7,9 +7,17 @@ import {
   AnyApiFactory,
   configApiRef,
   createApiFactory,
+  discoveryApiRef
 } from '@backstage/core-plugin-api';
+import { catalogApiRef } from '@backstage/plugin-catalog-react';
+import { CatalogClient } from '@backstage/catalog-client';
 
 export const apis: AnyApiFactory[] = [
+  createApiFactory({
+    api: catalogApiRef,
+    deps: { discoveryApi: discoveryApiRef },
+    factory: ({ discoveryApi }) => new CatalogClient({ discoveryApi })
+  }),
   createApiFactory({
     api: scmIntegrationsApiRef,
     deps: { configApi: configApiRef },
--- packages/app/src/components/Home/Home.tsx
+++ packages/app/src/components/Home/Home.tsx
@@ -1,6 +1,72 @@
-import React from 'react';
-import { PrimaryButton } from '@fluentui/react';
+import React, { useMemo } from 'react';
+import {
+  DetailsList,
+  MessageBar,
+  Spinner,
+  MessageBarType,
+  IColumn,
+  DetailsListLayoutMode,
+} from '@fluentui/react';
+import { catalogApiRef } from '@backstage/plugin-catalog-react';
+import { useApi } from '@backstage/core-plugin-api';
+import useAsync from 'react-use/lib/useAsync';
+
+const columns: IColumn[] = [
+  {
+    key: 'column1',
+    name: 'Name',
+    fieldName: 'name',
+    minWidth: 100,
+    maxWidth: 250,
+  },
+  {
+    key: 'column2',
+    name: 'Description',
+    fieldName: 'description',
+    minWidth: 100,
+    maxWidth: 250,
+  },
+];
 
 export const Home = () => {
-    return <PrimaryButton>Press me!</PrimaryButton>
-}
+  const catalogApi = useApi(catalogApiRef);
+  const {
+    loading,
+    value = { items: [] },
+    error,
+  } = useAsync(() =>
+    catalogApi.getEntities({
+      filter: {
+        kind: 'Component',
+      },
+    }),
+  );
+
+  const items = useMemo(
+    () =>
+      value.items.map(item => ({
+        key: item.metadata.uid,
+        name: item.metadata.name,
+        description: item.metadata.description,
+      })),
+    [value],
+  );
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    <MessageBar messageBarType={MessageBarType.error}>
+      Encountered an error while fetching records from the catalog.
+    </MessageBar>;
+  }
+
+  return (
+    <DetailsList
+      columns={columns}
+      items={items}
+      layoutMode={DetailsListLayoutMode.justified}
+    />
+  );
+};
